102e5f02fbf978993ff8497f0695c4e62a37bf58
AxelanO7 <axel@btwedutech.com>
2024-07-18 02:42:42 +0800

    feat: Add user profile retrieval and display in navbar

--- src/layouts/navbar.tsx
+++ src/layouts/navbar.tsx
@@ -8,9 +8,14 @@ import {
 } from "@sc/components//ui/popover";
 import { ArrowRightStartOnRectangleIcon } from "@heroicons/react/20/solid";
 import clsx from "clsx";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { getBaseUrl } from "@/helpers/api";
+import { UserProps } from "@/types/user";
 
 const Navbar = () => {
   const [sidebarOpen, setSidebarOpen] = useRecoilState(sidebarState);
+  const [user, setUser] = useState<UserProps>();
 
   const toogleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
@@ -20,6 +25,27 @@ const Navbar = () => {
     localStorage.clear();
     window.location.href = "/login";
   };
+
+  const getUserProfile = () => {
+    axios
+      .get(`${getBaseUrl()}/user/private/profile`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+        const dataRes: UserProps = res.data.data;
+        setUser(dataRes);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  useEffect(() => {
+    getUserProfile();
+  }, []);
 
   return (
     <>
@@ -31,7 +57,12 @@ const Navbar = () => {
         <Popover>
           <PopoverTrigger className="flex items-center">
             <div className="h-8 w-8 bg-gray-300 rounded-full flex items-center justify-center" />
-            <p className="ml-4">User</p>
+            <p className="ml-2 text-sm">
+              {user?.username
+                ?.split(" ")
+                .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+                .join(" ")}
+            </p>
           </PopoverTrigger>
           <PopoverContent
             className="bg-white shadow-md rounded-lg p-0"
