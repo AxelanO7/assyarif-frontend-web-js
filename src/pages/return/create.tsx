6194da0c0dd3457e16571251b871855c14ea87fa
AxelanO7 <jeremia123.jm@gmail.com>
2024-12-12 01:33:06 +0800

    feat(return): comment out file upload section in create return page

--- src/pages/return/create.tsx
+++ src/pages/return/create.tsx
@@ -275,15 +275,14 @@ const CreateReturn = () => {
                 </select>
               </div>
             </div>
-            {/* TODO: change into no invoice */}
-            <div className="mt-4 flex flex-col">
+            {/* <div className="mt-4 flex flex-col">
               <p>Upload Bukti</p>
               <Input
                 type="file"
                 className="w-max"
                 onChange={(e) => setProof(e.target.files![0])}
               />
-            </div>
+            </div> */}
             <div className="flex justify-end w-full">
               <button
                 className="bg-blue-500 text-white px-3 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 mt-4"
