ee477fe04887aae074cc78ffd124ff9757a633d3
AxelanO7 <jeremia123.jm@gmail.com>
2025-01-19 03:42:47 +0800

    fix(outcome): remove empty state message from TableBody in CreateOutcome component

--- src/pages/outcome/create.tsx
+++ src/pages/outcome/create.tsx
@@ -336,15 +336,7 @@ const CreateOutcome = () => {
                   </TableHead>
                 </TableRow>
               </TableHeader>
-              <TableBody
-                onEmptied={() => (
-                  <TableRow>
-                    <TableCell colSpan={8} className="text-center">
-                      Tidak ada data
-                    </TableCell>
-                  </TableRow>
-                )}
-              >
+              <TableBody>
                 {finalOrders?.map((order) => (
                   <TableRow key={order.orders.id}>
                     <TableCell className="border-2 border-gray-300 p-2 text-center">
