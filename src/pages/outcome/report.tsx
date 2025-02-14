17998411ea5dc915690435a2eb0940be5f5a2451
AxelanO7 <jeremia123.jm@gmail.com>
2025-02-15 05:20:42 +0800

    ✨ feat(report): add print functionality to outcome report
    - implement handleTapPrint function to print the outcome report table
    - include signature section with "Kepala Toko" and "Syarif"
    - add "Cetak" button to trigger the print functionality

--- src/pages/outcome/report.tsx
+++ src/pages/outcome/report.tsx
@@ -52,6 +52,43 @@ const ReportOutcome = () => {
     setFilteredOuts(filtered);
   };
 
+  const handleTapPrint = () => {
+    const printTable = document.getElementById("printTable");
+    if (printTable) {
+      const printContents = printTable.innerHTML;
+      const originalContents = document.body.innerHTML;
+      document.body.innerHTML = printContents;
+      // make empty space for image signature
+      const emptySpace = document.createElement("div");
+      emptySpace.style.height = "200px";
+      document.body.appendChild(emptySpace);
+      // add  signature
+      const divParent = document.createElement("div");
+      divParent.style.position = "absolute";
+      divParent.style.right = "10px";
+      divParent.style.bottom = "10px";
+      divParent.style.fontSize = "12px";
+
+      divParent.innerHTML = `
+        <p class="font-bold text-center">Mengetahui</p>
+        <p class="font-bold text-center">Kepala Toko</p>
+        <br>
+        <br>
+        <br>
+        <br>
+        <p class="font-bold text-center">Syarif</p>
+      `;
+
+      document.body.appendChild(divParent);
+
+      window.print();
+      document.body.innerHTML = originalContents;
+      printTable.innerHTML = printContents;
+      // close the dialog
+      window.location.reload();
+    }
+  };
+
   const handleTapDetail = (outs: Out[]) => {
     return (
       <Dialog>
@@ -63,84 +100,94 @@ const ReportOutcome = () => {
         <DialogContent className="max-w-[90%]">
           <DialogHeader>
             <DialogTitle>Detail Barang Keluar</DialogTitle>
-            <>
-              <Table className="w-full mt-4">
-                <TableHeader>
-                  <TableRow>
-                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
-                      No
-                    </TableHead>
-                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
-                      Tanggal
-                    </TableHead>
-                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
-                      Nama Outlet
-                    </TableHead>
-                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
-                      Nama
-                    </TableHead>
-                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
-                      Jumlah Barang
-                    </TableHead>
-                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
-                      Total
-                    </TableHead>
-                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
-                      Bayar
-                    </TableHead>
-                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
-                      Kembalian
-                    </TableHead>
-                  </TableRow>
-                </TableHeader>
-                <TableBody>
-                  {outs.map((out, index) => (
-                    <TableRow key={index}>
-                      <TableCell className="border-2 border-gray-300 p-2 text-center">
-                        {index++}
-                      </TableCell>
-                      <TableCell className="border-2 border-gray-300 p-2 text-center">
-                        {new Date(
-                          out.created_at?.toString() || new Date()
-                        ).toLocaleDateString("id-ID", {
-                          weekday: "long",
-                          year: "numeric",
-                          month: "long",
-                          day: "numeric",
-                        })}
-                      </TableCell>
-                      <TableCell className="border-2 border-gray-300 p-2 text-center">
-                        {out.order.outlet.name}
-                      </TableCell>
-                      <TableCell className="border-2 border-gray-300 p-2 text-center">
-                        {out.order.stock.name}
-                      </TableCell>
-                      <TableCell className="border-2 border-gray-300 p-2 text-center">
-                        {out.order.total_order}
-                      </TableCell>
-                      <TableCell className="border-2 border-gray-300 p-2 text-center">
-                        {out.order.total_paid.toLocaleString("id-ID", {
-                          style: "currency",
-                          currency: "IDR",
-                        })}
-                      </TableCell>
-                      <TableCell className="border-2 border-gray-300 p-2 text-center">
-                        {out.total_paided.toLocaleString("id-ID", {
-                          style: "currency",
-                          currency: "IDR",
-                        })}
-                      </TableCell>
-                      <TableCell className="border-2 border-gray-300 p-2 text-center">
-                        {out.return_cash.toLocaleString("id-ID", {
-                          style: "currency",
-                          currency: "IDR",
-                        })}
-                      </TableCell>
+            <div>
+              <div id="printTable">
+                <Table className="w-full mt-4">
+                  <TableHeader>
+                    <TableRow>
+                      <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                        No
+                      </TableHead>
+                      <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                        Tanggal
+                      </TableHead>
+                      <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                        Nama Outlet
+                      </TableHead>
+                      <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                        Nama
+                      </TableHead>
+                      <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                        Jumlah Barang
+                      </TableHead>
+                      <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                        Total
+                      </TableHead>
+                      <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                        Bayar
+                      </TableHead>
+                      <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                        Kembalian
+                      </TableHead>
                     </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </>
+                  </TableHeader>
+                  <TableBody>
+                    {outs.map((out, index) => (
+                      <TableRow key={index}>
+                        <TableCell className="border-2 border-gray-300 p-2 text-center">
+                          {index++}
+                        </TableCell>
+                        <TableCell className="border-2 border-gray-300 p-2 text-center">
+                          {new Date(
+                            out.created_at?.toString() || new Date()
+                          ).toLocaleDateString("id-ID", {
+                            weekday: "long",
+                            year: "numeric",
+                            month: "long",
+                            day: "numeric",
+                          })}
+                        </TableCell>
+                        <TableCell className="border-2 border-gray-300 p-2 text-center">
+                          {out.order.outlet.name}
+                        </TableCell>
+                        <TableCell className="border-2 border-gray-300 p-2 text-center">
+                          {out.order.stock.name}
+                        </TableCell>
+                        <TableCell className="border-2 border-gray-300 p-2 text-center">
+                          {out.order.total_order}
+                        </TableCell>
+                        <TableCell className="border-2 border-gray-300 p-2 text-center">
+                          {out.order.total_paid.toLocaleString("id-ID", {
+                            style: "currency",
+                            currency: "IDR",
+                          })}
+                        </TableCell>
+                        <TableCell className="border-2 border-gray-300 p-2 text-center">
+                          {out.total_paided.toLocaleString("id-ID", {
+                            style: "currency",
+                            currency: "IDR",
+                          })}
+                        </TableCell>
+                        <TableCell className="border-2 border-gray-300 p-2 text-center">
+                          {out.return_cash.toLocaleString("id-ID", {
+                            style: "currency",
+                            currency: "IDR",
+                          })}
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </div>
+              <div className="flex justify-end mt-4">
+                <Button
+                  className="bg-c-dark-blue rounded-md px-3 text-white"
+                  onClick={handleTapPrint}
+                >
+                  Cetak
+                </Button>
+              </div>
+            </div>
           </DialogHeader>
         </DialogContent>
       </Dialog>
@@ -225,3 +272,11 @@ const ReportOutcome = () => {
 };
 
 export default ReportOutcome;
+function useReactToPrint(arg0: {
+  content: () => any;
+  pageStyle: string;
+  documentTitle: string;
+  onAfterPrint: () => void;
+}) {
+  throw new Error("Function not implemented.");
+}
