49a6e9bad615bd1393bf8506c51b1397341e117a
AxelanO7 <jeremia123.jm@gmail.com>
2025-02-15 04:10:51 +0800

    ✨ feat(income): display income report with detail dialog
    - change state stocks from Stuff[] to PeriodIn[]
    - change endpoint from /stuff/in to /stuff/period/in
    - add detail dialog to show detail of income
    - change table to show date and action only
    - add getFormatDate helper to format date

--- src/pages/income/report.tsx
+++ src/pages/income/report.tsx
@@ -3,8 +3,7 @@ import BaseLayout from "../../layouts/base";
 import { getBaseUrl } from "../../helpers/api";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Stuff } from "../../types/stuff";
-import Swal from "sweetalert2";
+import { PeriodIn, Stuff } from "../../types/stuff";
 import {
   Table,
   TableBody,
@@ -13,12 +12,19 @@ import {
   TableHeader,
   TableRow,
 } from "@/shadcn/components/ui/table";
-import { Input } from "@/shadcn/components/ui/input";
 import { Button } from "@nextui-org/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/shadcn/components/ui/dialog";
+import { DialogTrigger } from "@radix-ui/react-dialog";
+import { getFormatDate } from "@/helpers/date";
 
 const ReportIncome = () => {
-  const [stocks, setStocks] = useState<Stuff[]>([]);
-  const [filteredStocks, setFilteredStocks] = useState<Stuff[]>([]);
+  const [stocks, setStocks] = useState<PeriodIn[]>([]);
+  const [filteredStocks, setFilteredStocks] = useState<PeriodIn[]>([]);
   const [search, setSearch] = useState("");
 
   const baseUrl = () => {
@@ -27,10 +33,10 @@ const ReportIncome = () => {
 
   const getIns = () => {
     axios
-      .get(`${baseUrl()}/stuff/in`)
+      .get(`${baseUrl()}/stuff/period/in`)
       .then((res) => {
         console.log(res.data);
-        const data: Stuff[] = res.data.data;
+        const data: PeriodIn[] = res.data.data;
         setStocks(data);
         setFilteredStocks(data);
       })
@@ -41,84 +47,92 @@ const ReportIncome = () => {
 
   const handleTapSearch = () => {
     const filtered = stocks.filter((stock) => {
-      return stock.name.toLowerCase().includes(search.toLowerCase());
+      return stock.date.toLowerCase().includes(search.toLowerCase());
     });
     setFilteredStocks(filtered);
   };
 
-  const addIn = () => {
-    window.location.href = "/in/add";
-  };
-
-  const editIn = (id: number) => {
-    window.location.href = `/in/${id}`;
-  };
-
-  const handleTapDetail = async (id: number) => {
-    await axios
-      .get(`${baseUrl()}/stuff/in/${id}`)
-      .then((res) => {
-        console.log(res.data);
-        const data: Stuff = res.data.data;
-        Swal.fire({
-          title: "Detail Barang",
-          html: `
-            <div class="text-left">
-              <p><span class="font-semibold">ID Barang:</span> ${
-                data.id_stuff
-              }</p>
-              <p><span class="font-semibold">Nama:</span> ${data.name}</p>
-              <p><span class="font-semibold">Jenis:</span> ${data.type}</p>
-              <p><span class="font-semibold">Jumlah:</span> ${data.quantity}</p>
-              <p><span class="font-semibold">Satuan:</span> ${data.unit}</p>
-              <p><span class="font-semibold">Harga:</span> ${data.price.toLocaleString(
-                "id-ID",
-                {
-                  style: "currency",
-                  currency: "IDR",
-                }
-              )}</p>
-            </div>
-          `,
-          confirmButtonColor: "#3085d6",
-        }).then((result) => {
-          if (result.isConfirmed) {
-            console.log("OK");
-          }
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  const handleDeleteIn = (id: number) => {
-    Swal.fire({
-      title: "Apakah Anda yakin?",
-      text: "Data yang dihapus tidak dapat dikembalikan!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Ya, hapus!",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        deleteStuff(id);
-      }
-    });
-  };
-
-  const deleteStuff = (id: number) => {
-    axios
-      .delete(`${baseUrl()}/stuff/in/${id}`)
-      .then((res) => {
-        Swal.fire("Berhasil!", "Data berhasil dihapus.", "success");
-        getIns();
-      })
-      .catch((err) => {
-        console.log(err);
-        Swal.fire("Gagal!", "Data gagal dihapus.", "error");
-      });
+  const handleTapDetail = (ins: Stuff[]) => {
+    return (
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button className="bg-c-dark-blue rounded-md px-3 text-white">
+            Detail
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="max-w-[90%]">
+          <DialogHeader>
+            <DialogTitle>Detail Barang Masuk</DialogTitle>
+            <>
+              <Table className="w-full mt-4">
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                      No
+                    </TableHead>
+                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                      Tanggal
+                    </TableHead>
+                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                      Nama
+                    </TableHead>
+                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                      Jenis
+                    </TableHead>
+                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                      Satuan
+                    </TableHead>
+                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                      Quantity
+                    </TableHead>
+                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                      Total
+                    </TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {ins.map((stock, index) => (
+                    <TableRow key={index}>
+                      <TableCell className="border-2 border-gray-300 p-2 text-black text-center">
+                        {index + 1}
+                      </TableCell>
+                      <TableCell className="border-2 border-gray-300 p-2 text-black text-center">
+                        {new Date(
+                          stock.created_at?.toString() || new Date()
+                        ).toLocaleDateString("id-ID", {
+                          weekday: "long",
+                          year: "numeric",
+                          month: "long",
+                          day: "numeric",
+                        })}
+                      </TableCell>
+                      <TableCell className="border-2 border-gray-300 p-2 text-black text-center">
+                        {stock.name}
+                      </TableCell>
+                      <TableCell className="border-2 border-gray-300 p-2 text-black text-center">
+                        {stock.type}
+                      </TableCell>
+                      <TableCell className="border-2 border-gray-300 p-2 text-black text-center">
+                        {stock.unit}
+                      </TableCell>
+                      <TableCell className="border-2 border-gray-300 p-2 text-black text-center">
+                        {stock.quantity}
+                      </TableCell>
+                      <TableCell className="border-2 border-gray-300 p-2 text-black text-center">
+                        {stock.price.toLocaleString("id-ID", {
+                          style: "currency",
+                          currency: "IDR",
+                        })}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    );
   };
 
   useEffect(() => {
@@ -128,28 +142,17 @@ const ReportIncome = () => {
   return (
     <>
       <BaseLayout>
-        <h1 className="text-3xl font-bold mx-6 pt-4">Barang Masuk</h1>
+        <h1 className="text-3xl font-bold mx-6 pt-4">Laporan Barang Masuk</h1>
         <div className="flex items-center bg-gray-300 px-6 py-2">
           <HomeIcon className="w-5 h-5" />
-          <p className="ml-2 font-semibold">Barang Masuk</p>
+          <p className="ml-2 font-semibold">Laporan Barang Masuk</p>
         </div>
         <div className="px-6">
           <div className="mt-4 bg-gray-200 px-8 py-8 rounded-md shadow-md">
             <h3 className="text-3xl font-semibold text-gray-500">
-              Barang Masuk
+              Laporan Barang Masuk
             </h3>
-            <div className="flex justify-between mt-4">
-              <div className="flex space-x-4 text-base font-semibold text-white">
-                {/* <button
-                  className="bg-c-dark-blue rounded-md px-3"
-                  onClick={addIn}
-                >
-                  Tambah Barang
-                </button> */}
-                {/* <button className="bg-c-yellow rounded-md px-3">
-                  Cetak Laporan
-                </button> */}
-              </div>
+            <div className="flex justify-end mt-4">
               <div className="flex items-center">
                 <input
                   type="search"
@@ -166,28 +169,13 @@ const ReportIncome = () => {
             <Table className="w-full mt-4">
               <TableHeader>
                 <TableRow>
-                  <TableHead className="border-2 border-gray-300 p-2 text-black text-center w-20">
-                    ID
+                  <TableHead className="border-2 border-gray-300 p-2 text-black text-center w-16">
+                    No
                   </TableHead>
                   <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
                     Tanggal
                   </TableHead>
-                  <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
-                    Nama
-                  </TableHead>
-                  <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
-                    Jenis
-                  </TableHead>
-                  <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
-                    Satuan
-                  </TableHead>
                   <TableHead className="border-2 border-gray-300 p-2 text-black text-center w-32">
-                    Quantity
-                  </TableHead>
-                  <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
-                    Total
-                  </TableHead>
-                  <TableHead className="border-2 border-gray-300 p-2 text-black text-center w-24">
                     Aksi
                   </TableHead>
                 </TableRow>
@@ -196,59 +184,23 @@ const ReportIncome = () => {
                 {filteredStocks.length === 0 ? (
                   <TableRow>
                     <TableCell
-                      colSpan={7}
-                      className="border-2 border-gray-300 p-2"
+                      colSpan={3}
+                      className="border-2 border-gray-300 p-2 text-center"
                     >
                       Data tidak ditemukan
                     </TableCell>
                   </TableRow>
                 ) : (
-                  filteredStocks.map((stock) => (
-                    <TableRow key={stock.id}>
+                  filteredStocks.map((ins, index) => (
+                    <TableRow key={index}>
                       <TableCell className="border-2 border-gray-300 p-2 text-center">
-                        {stock.id_stuff}
+                        {index + 1}
                       </TableCell>
                       <TableCell className="border-2 border-gray-300 p-2 text-center">
-                        {new Date(
-                          stock.created_at?.toString() || new Date()
-                        ).toLocaleDateString("id-ID", {
-                          weekday: "long",
-                          year: "numeric",
-                          month: "long",
-                          day: "numeric",
-                        })}
+                        {getFormatDate(ins.date)}
                       </TableCell>
                       <TableCell className="border-2 border-gray-300 p-2 text-center">
-                        {stock.name}
-                      </TableCell>
-                      <TableCell className="border-2 border-gray-300 p-2 text-center">
-                        {stock.type}
-                      </TableCell>
-                      <TableCell className="border-2 border-gray-300 p-2 text-center">
-                        {stock.unit}
-                      </TableCell>
-                      <TableCell className="border-2 border-gray-300 p-2 text-center">
-                        {stock.quantity}
-                      </TableCell>
-                      <TableCell className="border-2 border-gray-300 p-2 text-center">
-                        {stock.price.toLocaleString("id-ID", {
-                          style: "currency",
-                          currency: "IDR",
-                        })}
-                      </TableCell>
-                      <TableCell className="border-2 border-gray-300 space-x-2 text-white font-semibold">
-                        <div className="w-full flex justify-center">
-                          {/* <PencilIcon
-                            className="w-5 h-5 cursor-pointer text-black"
-                            onClick={() => editIn(stock.id)}
-                          /> */}
-                          <Button
-                            onClick={() => handleTapDetail(stock.id)}
-                            className="bg-c-dark-blue rounded-md px-3 text-white"
-                          >
-                            Detail
-                          </Button>
-                        </div>
+                        {handleTapDetail(ins.ins)}
                       </TableCell>
                     </TableRow>
                   ))
