52f8d37f5c2bf3c0a8c901d44e174f9da2dc5ab3
AxelanO7 <jeremia123.jm@gmail.com>
2025-02-15 02:00:15 +0800

    ✨ feat(outcome): display report outcome data in table
    - add table component to display outcome data
    - integrate API to get period outcome data
    - add search functionality to filter outcome data by date
    - add detail dialog to show detail outcome data

--- src/pages/outcome/report.tsx
+++ src/pages/outcome/report.tsx
@@ -1,10 +1,9 @@
 import { HomeIcon, MagnifyingGlassIcon } from "@heroicons/react/20/solid";
 import BaseLayout from "../../layouts/base";
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { getBaseUrl } from "../../helpers/api";
-import { OutProps } from "../../types/stuff";
-import Swal from "sweetalert2";
+import { Out, PeriodOut } from "../../types/stuff";
 import {
   Table,
   TableBody,
@@ -14,10 +13,17 @@ import {
   TableRow,
 } from "@/shadcn/components/ui/table";
 import { Button } from "@nextui-org/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/shadcn/components/ui/dialog";
 
 const ReportOutcome = () => {
-  const [outs, setOuts] = useState<OutProps[]>([]);
-  const [filteredOuts, setFilteredOuts] = useState<OutProps[]>([]);
+  const [outs, setOuts] = useState<PeriodOut[]>([]);
+  const [filteredOuts, setFilteredOuts] = useState<PeriodOut[]>([]);
   const [search, setSearch] = useState("");
 
   const baseUrl = () => {
@@ -26,10 +32,10 @@ const ReportOutcome = () => {
 
   const getOuts = () => {
     axios
-      .get(`${baseUrl()}/stuff/out`)
+      .get(`${baseUrl()}/stuff/period`)
       .then((res) => {
         console.log(res.data);
-        const data: OutProps[] = res.data.data;
+        const data: PeriodOut[] = res.data.data;
         setOuts(data);
         setFilteredOuts(data);
       })
@@ -40,197 +46,57 @@ const ReportOutcome = () => {
 
   const handleTapSearch = () => {
     const filtered = outs.filter((out) => {
-      return out.order.outlet.name.toLowerCase().includes(search.toLowerCase());
+      return out.date.toLowerCase().includes(search.toLowerCase());
     });
     setFilteredOuts(filtered);
   };
 
-  const addOut = () => {
-    window.location.href = "/out/add";
-  };
-
-  const editOut = (id: number) => {
-    window.location.href = `/out/${id}`;
-  };
-
-  const handleDeleteOut = (id: number) => {
-    Swal.fire({
-      title: "Apakah Anda yakin?",
-      text: "Data yang dihapus tidak dapat dikembalikan!",
-      icon: "warning",
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Ya, hapus!",
-    }).then((result) => {
-      if (result.isConfirmed) {
-        deleteStuff(id);
-      }
-    });
-  };
-
-  const deleteStuff = (id: number) => {
-    axios
-      .delete(`${baseUrl()}/stuff/out/${id}`)
-      .then((res) => {
-        console.log(res.data);
-        getOuts();
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  const handleTapDetail = async (id: number) => {
-    await axios
-      .get(`${baseUrl()}/stuff/out/${id}`)
-      .then((res) => {
-        console.log(res.data);
-        const data: OutProps = res.data.data;
-        Swal.fire({
-          title: "Detail Barang",
-          html: `
-            <div class="text-left">
-              <p><span class="font-semibold">ID Barang:</span> ${
-                data.out_id
-              }</p>
-              <p><span class="font-semibold">Tanggal:</span> ${new Date(
-                data.created_at?.toString() || new Date()
-              ).toLocaleDateString("id-ID", {
-                weekday: "long",
-                year: "numeric",
-                month: "long",
-                day: "numeric",
-              })}</p>
-              <p><span class="font-semibold">Nama Outlet:</span> ${
-                data.order.outlet.name
-              }</p>
-              <p><span class="font-semibold">Nama:</span> ${
-                data.order.stock.name
-              }</p>
-              <p><span class="font-semibold">Jumlah Barang:</span> ${
-                data.order.total_order
-              }</p>
-              <p><span class="font-semibold">Total:</span> ${data.order.total_paid.toLocaleString(
-                "id-ID",
-                {
-                  style: "currency",
-                  currency: "IDR",
-                }
-              )}</p>
-              <p><span class="font-semibold">Bayar:</span> ${data.total_paided.toLocaleString(
-                "id-ID",
-                {
-                  style: "currency",
-                  currency: "IDR",
-                }
-              )}</p>
-              <p><span class="font-semibold">Kembalian:</span> ${data.return_cash.toLocaleString(
-                "id-ID",
-                {
-                  style: "currency",
-                  currency: "IDR",
-                }
-              )}</p>
-            </div>
-          `,
-          confirmButtonColor: "#3085d6",
-        }).then((result) => {
-          if (result.isConfirmed) {
-            console.log("OK");
-          }
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  useEffect(() => {
-    getOuts();
-  }, []);
-  return (
-    <>
-      <BaseLayout>
-        <h1 className="text-3xl font-bold mx-6 pt-4">Barang Keluar</h1>
-        <div className="flex items-center bg-gray-300 px-6 py-2">
-          <HomeIcon className="w-5 h-5" />
-          <p className="ml-2 font-semibold">Barang Keluar</p>
-        </div>
-        <div className="px-6">
-          <div className="mt-4 bg-gray-200 px-8 py-8 rounded-md shadow-md">
-            <h3 className="text-3xl font-semibold text-gray-500">
-              Barang Keluar
-            </h3>
-            <div className="flex justify-between mt-4">
-              <div className="flex space-x-4 text-base font-semibold text-white">
-                {/* <button
-                  className="bg-c-dark-blue rounded-md px-3"
-                  onClick={addOut}
-                >
-                  Tambah Barang
-                </button> */}
-              </div>
-              <div className="flex items-center">
-                <input
-                  type="search"
-                  className="p-2 border-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  placeholder="Cari outlet"
-                  onChange={(e) => setSearch(e.target.value)}
-                />
-                <MagnifyingGlassIcon
-                  className="w-10 h-10 text-white bg-c-dark-blue rounded-md p-2 ml-2 cursor-pointer"
-                  onClick={handleTapSearch}
-                />
-              </div>
-            </div>
-            <Table className="w-full mt-4">
-              <TableHeader>
-                <TableRow>
-                  <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
-                    ID
-                  </TableHead>
-                  <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
-                    Tanggal
-                  </TableHead>
-                  <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
-                    Nama Outlet
-                  </TableHead>
-                  <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
-                    Nama
-                  </TableHead>
-                  <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
-                    Jumlah Barang
-                  </TableHead>
-                  <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
-                    Total
-                  </TableHead>
-                  <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
-                    Bayar
-                  </TableHead>
-                  <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
-                    Kembalian
-                  </TableHead>
-                  <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
-                    Aksi
-                  </TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {filteredOuts.length === 0 ? (
+  const handleTapDetail = (outs: Out[]) => {
+    return (
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button className="bg-c-dark-blue rounded-md px-3 text-white">
+            Detail
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="max-w-[90%]">
+          <DialogHeader>
+            <DialogTitle>Detail Barang Keluar</DialogTitle>
+            <>
+              <Table className="w-full mt-4">
+                <TableHeader>
                   <TableRow>
-                    <TableCell
-                      colSpan={8}
-                      className="border-2 border-gray-300 p-2 text-center"
-                    >
-                      Data tidak ditemukan
-                    </TableCell>
+                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                      No
+                    </TableHead>
+                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                      Tanggal
+                    </TableHead>
+                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                      Nama Outlet
+                    </TableHead>
+                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                      Nama
+                    </TableHead>
+                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                      Jumlah Barang
+                    </TableHead>
+                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                      Total
+                    </TableHead>
+                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                      Bayar
+                    </TableHead>
+                    <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                      Kembalian
+                    </TableHead>
                   </TableRow>
-                ) : (
-                  filteredOuts.map((out) => (
-                    <TableRow key={out.id}>
+                </TableHeader>
+                <TableBody>
+                  {outs.map((out, index) => (
+                    <TableRow key={index}>
                       <TableCell className="border-2 border-gray-300 p-2 text-center">
-                        {out.out_id}
+                        {index++}
                       </TableCell>
                       <TableCell className="border-2 border-gray-300 p-2 text-center">
                         {new Date(
@@ -269,13 +135,104 @@ const ReportOutcome = () => {
                           currency: "IDR",
                         })}
                       </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    );
+  };
+
+  const listMonth = (month: string) => {
+    const months = [
+      "Januari",
+      "Februari",
+      "Maret",
+      "April",
+      "Mei",
+      "Juni",
+      "Juli",
+      "Agustus",
+      "September",
+      "Oktober",
+      "November",
+      "Desember",
+    ];
+    return months[parseInt(month) - 1];
+  };
+
+  const getFormatDate = (date: string) => {
+    const dateArr = date.split("-");
+    return `${listMonth(dateArr[0])} ${dateArr[1]}`;
+  };
+
+  useEffect(() => {
+    getOuts();
+  }, []);
+  return (
+    <>
+      <BaseLayout>
+        <h1 className="text-3xl font-bold mx-6 pt-4">Laporan Barang Keluar</h1>
+        <div className="flex items-center bg-gray-300 px-6 py-2">
+          <HomeIcon className="w-5 h-5" />
+          <p className="ml-2 font-semibold">Laporan Barang Keluar</p>
+        </div>
+        <div className="px-6">
+          <div className="mt-4 bg-gray-200 px-8 py-8 rounded-md shadow-md">
+            <h3 className="text-3xl font-semibold text-gray-500">
+              Laporan Barang Keluar
+            </h3>
+            <div className="flex justify-end mt-4">
+              <div className="flex items-center">
+                <input
+                  className="p-2 border-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  placeholder="Cari outlet"
+                  onChange={(e) => setSearch(e.target.value)}
+                />
+                <MagnifyingGlassIcon
+                  className="w-10 h-10 text-white bg-c-dark-blue rounded-md p-2 ml-2 cursor-pointer"
+                  onClick={handleTapSearch}
+                />
+              </div>
+            </div>
+            <Table className="w-full mt-4">
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="border-2 border-gray-300 p-2 text-black text-center w-16">
+                    No
+                  </TableHead>
+                  <TableHead className="border-2 border-gray-300 p-2 text-black text-center">
+                    Tanggal
+                  </TableHead>
+                  <TableHead className="border-2 border-gray-300 p-2 text-black text-center w-32">
+                    Aksi
+                  </TableHead>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {filteredOuts.length === 0 ? (
+                  <TableRow>
+                    <TableCell
+                      colSpan={8}
+                      className="border-2 border-gray-300 p-2 text-center"
+                    >
+                      Data tidak ditemukan
+                    </TableCell>
+                  </TableRow>
+                ) : (
+                  filteredOuts.map((out, index) => (
+                    <TableRow>
                       <TableCell className="border-2 border-gray-300 p-2 text-center">
-                        <Button
-                          className="bg-c-dark-blue rounded-md px-3 text-white"
-                          onClick={() => handleTapDetail(out.id)}
-                        >
-                          Detail
-                        </Button>
+                        {index + 1}
+                      </TableCell>
+                      <TableCell className="border-2 border-gray-300 p-2 text-center">
+                        {getFormatDate(out.date)}
+                      </TableCell>
+                      <TableCell className="border-2 border-gray-300 p-2 text-center">
+                        {handleTapDetail(out.outs)}
                       </TableCell>
                     </TableRow>
                   ))
